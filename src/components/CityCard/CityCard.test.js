036a17abbef755c08c14bacfe77d1e2817ad1085
Laura Caroselli <lcaroselli@outlook.com>
2017-09-20 15:35:44 -0600

    Add component test to CityCard

--- src/components/CityCard/CityCard.test.js
+++ src/components/CityCard/CityCard.test.js
@@ -2,36 +2,45 @@ import React from 'react';
 import CityCard from './CityCard';
 import { withRouter } from 'react-router';
 import { shallow, mount } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
 describe('CityCard', () => {
   let wrapper;
+  let mountWrapper;
+  let shallowWrapper;
+  let store;
+
+  let initialState = { output:100 }
+  let mockStore = configureStore();
 
   beforeEach(() => {
+    store = mockStore(initialState);
     wrapper =  withRouter(<CityCard />);
+    shallowWrapper = shallow(<CityCard store={store}/>).dive();
+    mountWrapper = mount(<CityCard />);
   });
 
-  test ('should exist', () => {
+  test.skip ('should exist', () => {
     expect(wrapper).toBeDefined();
   });
 
   test.skip ('should render an image', () => {
-    wrapper = shallow(<CityCard />)
-    expect(wrapper.find('.header-image').length).toEqual(1);
+    expect(shallowWrapper.find('.header-image').length).toEqual(1);
   });
 
   test.skip ('should render a city summary', () => {
-    expect(wrapper.find('.summary-data').length).toEqual(1);
+    expect(shallowWrapper.find('.summary-data').length).toEqual(1);
   });
 
   test.skip ('should render score data', () => {
-    expect(wrapper.find('.score-data').length).toEqual(1);
+    expect(shallowWrapper.find('.score-data').length).toEqual(1);
   });
 
   test.skip ('should render a score chart', () => {
-    expect(wrapper.find('.charts').length).toEqual(1);
+    expect(shallowWrapper.find('.charts').length).toEqual(1);
   });
 
   test.skip ('should render cost-of-living data', () => {
-    expect(wrapper.find('.detail-data').length).toEqual(1);
+    expect(shallowWrapper.find('.detail-data').length).toEqual(1);
   });
 });
